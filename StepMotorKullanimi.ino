4dbb3062c4c88dae3a2f3ba7fc8ec90f83350e6b
Esat GÖZCÜ <35576161+esatgozcu@users.noreply.github.com>
2018-06-17 19:48:21 +0300

    Add files via upload

---
+++ StepMotorKullanimi.ino
@@ -0,0 +1,20 @@
+#include <X113647Stepper.h>
+
+static const int STEPS_PER_REVOLUTION = 1000; 
+
+X113647Stepper myStepper(STEPS_PER_REVOLUTION, 8, 9, 10, 11);
+
+void setup() {
+  
+  myStepper.setSpeed(10);
+}
+
+void loop() {
+  
+  myStepper.step(STEPS_PER_REVOLUTION);
+  delay(500);
+
+  myStepper.step(-STEPS_PER_REVOLUTION);
+  delay(500);
+}
+
